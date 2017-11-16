e05c2a047777026643e2eb1ce402e35f4cd6c0cc
sgj <764341699@qq.com>
2017-11-16 18:01:14 +0800

    音频

--- dist/js/h5Game/baseEvent.js
+++ dist/js/h5Game/baseEvent.js
@@ -3,15 +3,28 @@
     var main = {
         init: function () {
             main.eventBgMusic();
+            main.eventBagBtn();
+
+            main.setSwiper();
+            main.loading();
         },
+        // 页面加载是否完成
+        loading: function() {
+            //当页面加载状态改变的时候执行这个方法.
+            document.onreadystatechange = function() {
+                if(document.readyState == 'complete') //当页面加载状态
+                    document.querySelector('#musicBg').play();
+                    alert('wan');
+            };
+
+        },
+
         // 绑定背景音乐事件
         eventBgMusic: function() {
             var audio_dom = document.querySelector('#musicBg');
             var btn_dom = document.querySelector('#bg_muisc_btn');
 
             btn_dom.onclick = function () {
-                console.dir(audio_dom);
-
                 if (this.className.indexOf('muisc-play') >= 0) {
                     audio_dom.pause();
                     this.className = this.className.replace(' muisc-play', '');
@@ -19,6 +32,47 @@
                 else {
                     this.className = this.className + ' muisc-play';
                     audio_dom.play();
+                }
+            }
+        },
+        // 活动锦囊
+        eventBagBtn: function () {
+            var bag_btn_dom = document.querySelector('#bag_btn');
+            var desc_box_dom = document.querySelector('#desc_box');
+            var desc_box_close_dom = document.querySelector('#desc_box_close_btn');
+
+            bag_btn_dom.onclick = function () {
+                if (desc_box_dom.className.indexOf('bag-show') >= 0) {
+                    desc_box_dom.className = desc_box_dom.className.replace(' bag-show', '');
+                } else {
+                    desc_box_dom.className = desc_box_dom.className + ' bag-show';
+                }
+            }
+
+            desc_box_close_dom.onclick = function (e) {
+                e.preventDefault();
+                desc_box_dom.className = desc_box_dom.className.replace(' bag-show', '');
+                desc_box_dom.className = desc_box_dom.className.replace('bag-show', '');
+            }
+
+        },
+
+        setSwiper: function () {
+            var slideBarTip_dom = document.querySelector('.slideBarTip');
+            var desc_nav_btn_dom = document.querySelector('#desc_nav_btn');
+
+            var num = 0;
+            var li_dom = document.querySelectorAll("#desc_nav_btn li");
+            var sw = new Swiper(".swiper-container",{
+
+            });
+
+            for (var i = 0; i < li_dom.length; i++) {
+                li_dom[i].onclick = function (e) {
+                    e.preventDefault();
+                    num = parseInt(e.target.getAttribute('channel'));
+                    sw.slideTo(num, 500);
+                    slideBarTip_dom.style.left = desc_nav_btn_dom.clientWidth * (num / 3) + 'px';
                 }
             }
         }
---
+++ dist/js/h5Game/game.js
@@ -0,0 +1,50 @@
+
+(function() {
+
+    var main = {
+        openShake: true,
+        init: function () {
+            main.bindShakeEvent();
+        },
+        bindShakeEvent: function () {
+            //获取加速度信息
+            //通过监听上一步获取到的x, y, z 值在一定时间范围内的变化率，进行设备是否有进行晃动的判断。
+            //而为了防止正常移动的误判，需要给该变化率设置一个合适的临界值。
+            var SHAKE_THRESHOLD = 1000;
+            var last_update = 0;
+            var x, y, z, last_x = 0, last_y = 0, last_z = 0;
+            var audio_dom1 = document.querySelector('#clickmusic');
+            audio_dom1.play();
+            try {
+                if (window.DeviceMotionEvent) {
+                    window.addEventListener('devicemotion', function (eventData) {
+                        var acceleration =eventData.accelerationIncludingGravity;
+                        var curTime = new Date().getTime();
+                        if ((curTime-last_update)> 200) {
+                            var diffTime = curTime -last_update;
+                            last_update = curTime;
+                            x = acceleration.x;
+                            y = acceleration.y;
+                            z = acceleration.z;
+                            var speed = Math.abs(x +y + z - last_x - last_y - last_z) / diffTime * 10000;
+                            if (speed > SHAKE_THRESHOLD) {
+
+                                audio_dom1.play();
+                                alert("你中奖啦！22");  // Do something
+                            }
+                            last_x = x;
+                            last_y = y;
+                            last_z = z;
+                        }
+                    }, false);
+                }
+            }
+            catch (e) {
+                alert('不兼容');
+            }
+        }
+
+    };
+
+    main.init();
+})();
